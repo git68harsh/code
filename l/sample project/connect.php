61bd1ce39145e0f781abb02af7857aaa91f8de5f
Your Name <you@example.com>
2023-04-28 12:13:47 +0530

    j

---
+++ l/sample project/connect.php
@@ -0,0 +1,26 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '','db_contact');
+
+// get the post records
+$Username = $_POST['Username'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+// database insert SQL code
+$sql = "INSERT INTO `tbl_contact` (`Id`, `username`, `email`, `phone`, `password`) 
+
+VALUES ('0', '$Username', '$email', '0', '$pass')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Registration details recieved";
+}
+
+?>
